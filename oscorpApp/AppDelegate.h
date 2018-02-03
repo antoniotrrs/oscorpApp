36a1f4222e0159012e17ec1360d11248f765c285
Antonio <luisato.trrs@gmail.com>
2018-02-03 17:23:29 -0600

    Initial Commit

---
+++ oscorpApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oscorpApp
+//
+//  Created by Antonio Torres on 03/02/18.
+//  Copyright © 2018 Antonio Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oscorpApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oscorpApp
+//
+//  Created by Antonio Torres on 03/02/18.
+//  Copyright © 2018 Antonio Torres. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
